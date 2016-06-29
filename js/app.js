7fd163d903de8a317584f7bc2340531f824ee2a3
Developer <robdev@Robs-MacBook-Air.local>
2016-06-29 20:45:33 +0200

    Created Bootstrap design and created a form page so that users can upload words to the site

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var noun = [
+{"en":"Car", "de_1":"das", "de_2":"Auto", "de_3":"Autos"},
+{"en":"Book", "de_1":"das", "de_2":"Buch", "de_3":"Bücher"}
+];
+
+
+document.getElementById("enText_1").innerHTML = noun[0].en;
+
+document.getElementById("deText_1").innerHTML = noun[0].de_1;
+document.getElementById("deText_2").innerHTML = noun[0].de_2;
+document.getElementById("deText_3").innerHTML = noun[0].de_3;
+
+	
+
+;
+
+
